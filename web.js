400504f62dade27dde5caac689607c38ba7bbae7
Zachary Rose <zachary.arthur.rose@gmail.com>
2013-07-21 22:15:58 +0000

    Reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(
+    fs.readFileSync("index.html","utf-8")
+  );
 });
 
 var port = process.env.PORT || 5000;
